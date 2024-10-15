9668ac273e037b2462c16e834b8d09e1bde57f45
longtongster <sacha.van.weeren@gmail.com>
2024-10-15 19:27:42 +0200

    commit relates to video 192 about 6:30 minutes

--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -8,7 +8,12 @@ from cybulde.utils.data_utils import get_raw_data_with_version
 from cybulde.utils.utils import get_logger
 
 from dask.distributed import Client
+import dask.dataframe as dd
 from pathlib import Path
+
+def process_raw_data(df_partition, dataset_cleaner_manager) -> dd.core.Series:
+    return df_partition["text"].apply(dataset_cleaner_manager)
+
 
 
 # the directory `configs` contains the `data_processing_config.yaml`
@@ -35,21 +40,34 @@ def process_data(config: DataProcessingConfig) -> None:
         # # print(config.dataset_reader_manager)
         dataset_reader_manager = instantiate(config.dataset_reader_manager)
         dataset_cleaner_manager = instantiate(config.dataset_cleaner_manager)
+        
+        logger.info("Reading raw data ...")
+        df = dataset_reader_manager.read_data()
+        print(df.head())
+        
+        logger.info("Cleaning data ...")
+        # print(df)
+        df = df.assign(cleaned_text=df.map_partitions(process_raw_data, dataset_cleaner_manager=dataset_cleaner_manager, meta=("text","object")))
+        # clear
+        # df = df.compute()
+        print(df.head())
+        #print(df.head())
+        
         # print(dataset_reader_manager)
-        # print(dataset_reader_manager)
+        # print(dataset_reader_manager)clear
 
         # Read the data from ghc twitter, jtd
-        df = dataset_reader_manager.read_data().compute()
-        sample_df = df.sample(n=5)
-        for idx, row in sample_df.iterrows():
-            print(60 * "*")
-            print(idx)
-            text = row["text"]
-            print(len(text))
-            print(text)
-            cleaned_text = dataset_cleaner_manager(text)
-            print(cleaned_text)
-        # print(df.head())
+        # df = dataset_reader_manager.read_data().compute()
+        # sample_df = df.sample(n=5)
+        # for idx, row in sample_df.iterrows():
+        #     print(60 * "*")
+        #     print(idx)
+        #     text = row["text"]
+        #     print(len(text))
+        #     print(text)
+        #     cleaned_text = dataset_cleaner_manager(text)
+        #     print(cleaned_text)
+        # # print(df.head())
         # print(df.compute().shape)
         # # print(df["dataset_name"].unique().compute())
         # print(df.compute()['dataset_name'].unique())
